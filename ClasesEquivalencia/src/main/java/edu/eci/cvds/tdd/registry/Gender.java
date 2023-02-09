6ce7685cc4d491449103d6b0923c32cfa7b91995
unknown <brian.fajardo@labinfo.is.escuelaing.edu.co>
2023-02-09 17:51:18 -0500

    Lab3

---
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Gender.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.tdd.registry;
+
+public enum Gender {
+
+    MALE, FEMALE, UNIDENTIFIED;
+
+}
---
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/RegisterResult.java
@@ -0,0 +1,7 @@
+package edu.eci.cvds.tdd.registry;
+
+public enum RegisterResult {
+
+    DEAD, UNDERAGE, INVALID_AGE, VALID, DUPLICATED
+
+}
